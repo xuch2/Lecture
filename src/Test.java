09eed43c7f12b69aacd83dab97806af45ed757c3
Sanghoon Lee <gcccompil3r@gmail.com>
2019-10-24 19:09:07 +0900

    Git Pull Test

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Java with Git");
         System.out.println("Additional Edit");
+        System.out.println("Git Pull Test");
     }
 }
