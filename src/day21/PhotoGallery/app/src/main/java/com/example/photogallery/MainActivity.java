275d69d394f966500e4f06d01bd2a281a80fd2b1
silenc3502 <gcccompil3r@gmail.com>
2019-11-11 21:43:27 +0900

    21th Photo Gallery

--- src/day21/PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.java
+++ src/day21/PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.java
@@ -42,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
         imgLang = (ImageView)findViewById(R.id.ImgLang);
 
+        /* setOnCheckedChangeListener 는
+           setOnClickListener 와 동작 방식은 유사하다.
+           OnClickListener 는 클릭을 했을때 발생함
+           OnCheckedChangeListener 는
+           체크를 하거나 체크를 해제할 경우 동작함
+         */
         chkAgree.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -65,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /* getCheckedRadioButtonId() 는
+                   XML 리소스에 등록해놓은 id 값을 판정한다.
+                   이를 기반으로 setImageResource 로
+                   원하는 이미지를 띄워줄 수 있다.
+                   setImageResource() 를 사용할 것이라면
+                   반드시 res 폴더에 이미지들을 배치해야 한다.
+                 */
                 switch (rGroup1.getCheckedRadioButtonId()) {
                     case R.id.Rc:
                         imgLang.setImageResource(
@@ -90,3 +103,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+/* 문제 20. 아무런 이미지 파일을 10 개 정도 확보한다.
+            CheckBox 를 3 개 만든다.
+            1 번째 체크 박스는 오늘 작성한 예제처럼
+            눈에 보이게 할지 말지를 결정하도록 한다.
+            2 번째 체크 박스는 랜덤 여부를 판정한다.
+            즉, 1, 2 번 체크 박스를 체크하면
+            무작위로 그림파일이 보이게 만들면 된다.
+            (Button 을 하나 만들어서
+            누를 때마다 무작위로 보이게 만들어준다.)
+            3 번째 체크 박스는 순서대로 보이게 해준다.
+            결론: 1, 2 번은 무작위 출력
+                  1, 3 번은 순차 출력
+                  2, 3 번은 동작 불가능!
+                  1 번만 있을 경우엔 VISIBLE 이므로
+                  메뉴만 보여준다고 생각하면 됨
+                  2 번이나 3 번만 있을 경우엔
+                  아무것도 보이지 않음
+                  (1 번이 꺼져있기 때문) */
