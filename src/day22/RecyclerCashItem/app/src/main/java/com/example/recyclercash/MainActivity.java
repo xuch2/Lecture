b8df03139b27e237afd9e2dc15f2db0858c149fb
silenc3502 <gcccompil3r@gmail.com>
2019-11-12 21:44:50 +0900

    22th RecyclerView

--- src/day22/RecyclerCashItem/app/src/main/java/com/example/recyclercash/MainActivity.java
+++ src/day22/RecyclerCashItem/app/src/main/java/com/example/recyclercash/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.recyclercash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
     }
 }
---
+++ src/day22/RecyclerCashItem/app/src/main/java/com/example/recyclercash/OnProductItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.recyclercash;
+
+import android.view.View;
+
+public interface OnProductItemClickListener {
+    public void onItemClick(ProductAdapter.ViewHolder holder,
+                            View view,
+                            int position);
+}
