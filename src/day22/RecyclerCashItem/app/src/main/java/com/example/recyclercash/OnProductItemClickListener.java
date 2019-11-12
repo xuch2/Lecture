2fa774d69c3204cffc42b1263198340ad810ccf3
silenc3502 <gcccompil3r@gmail.com>
2019-11-12 14:49:51 +0900

    22 th

--- src/day22/RecyclerCashItem/app/src/main/java/com/example/recyclercash/OnProductItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.recyclercash;
-
-import android.view.View;
-
-public interface OnProductItemClickListener {
-    public void onItemClick(ProductAdapter.ViewHolder holder,
-                            View view,
-                            int position);
-}
