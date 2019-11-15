fb3d20b9ff76de4f8117d42a3a1c1618d9d12d39
silenc3502 <gcccompil3r@gmail.com>
2019-11-15 21:41:19 +0900

    25th Advanced Touch Event

--- src/day25/AdvTouch/app/src/main/java/com/example/advtouchevent/MainActivity.java
+++ src/day25/AdvTouch/app/src/main/java/com/example/advtouchevent/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.advtouchevent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1;
+    GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +40,79 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        detector = new GestureDetector(
+                this,
+                new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                println("Call onDown()");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                println("Call onShowPress()");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("Call onSingleTapUp()");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1,
+                                    MotionEvent e2,
+                                    float distanceX,
+                                    float distanceY) {
+                println("Call onScroll() : " + distanceX +
+                        ", " + distanceY);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                println("Call onLongPress()");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1,
+                                   MotionEvent e2,
+                                   float velocityX,
+                                   float velocityY) {
+                println("Call onFling() : " + velocityX +
+                        ", " + velocityY);
+                return true;
+            }
+        });
+
+        View v2 = (View)findViewById(R.id.view2);
+
+        v2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
 
     public void println(String data) {
         tv1.append(data + "\n");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(
+                    this,
+                    "You Pressed [BACK] Button",
+                    Toast.LENGTH_SHORT
+            ).show();
+
+            return true;
+        }
+        return false;
     }
 }
