8f9515972e8160a4e19c079bec38a953f86338fb
silenc3502 <gcccompil3r@gmail.com>
2019-11-19 08:22:17 +0900

    22 th & 23 th Recycler View

---
+++ src/day27/SQLFinish/app/src/main/java/com/example/sqltestfinish/InfoProvider.java
@@ -0,0 +1,110 @@
+package com.example.sqltestfinish;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class InfoProvider extends ContentProvider {
+    private static final String AUTHORITY = "com.example.sqlitetest";
+    private static final String BASE_PATH = "person";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    private static final int PERSONS = 1;
+    private static final int PERSON_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, PERSONS);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", PERSON_ID);
+    }
+
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        DBHelper helper = new DBHelper(getContext());
+        database = helper.getWritableDatabase();
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        Cursor cursor;
+        switch (uriMatcher.match(uri)) {
+            case PERSONS:
+                cursor =  database.query(DBHelper.TABLE_NAME, DBHelper.ALL_COLUMNS,
+                        s,null,null,null,DBHelper.PERSON_NAME +" ASC");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case PERSONS:
+                return "vnd.android.cursor.dir/persons";
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        long id = database.insert(DBHelper.TABLE_NAME, null, contentValues);
+
+        if (id > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, id);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+
+        throw new SQLException("Add Fail -> URI :" + uri);
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case PERSONS:
+                count =  database.delete(DBHelper.TABLE_NAME, s, strings);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case PERSONS:
+                count =  database.update(DBHelper.TABLE_NAME, contentValues, s, strings);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+}
---
+++ src/day27/SQLFinish/app/src/main/java/com/example/sqltestfinish/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.sqltestfinish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv);
+
+        Button button = findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertPerson();
+            }
+        });
+
+        Button button2 = findViewById(R.id.btn2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queryPerson();
+            }
+        });
+
+        Button button3 = findViewById(R.id.btn3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePerson();
+            }
+        });
+
+        Button button4 = findViewById(R.id.btn4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deletePerson();
+            }
+        });
+    }
+
+    public void insertPerson() {
+        println("insertPerson Called");
+
+        String uriString = "content://com.example.sqltestfinish/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        String[] columns = cursor.getColumnNames();
+        println("columns count -> " + columns.length);
+        for (int i = 0; i < columns.length; i++) {
+            println("#" + i + " : " + columns[i]);
+        }
+
+        ContentValues values = new ContentValues();
+        values.put("name", "Jessica");
+        values.put("age", 20);
+        values.put("mobile", "010-3333-3333");
+
+        uri = getContentResolver().insert(uri, values);
+        println("insert res -> " + uri.toString());
+    }
+
+    public void queryPerson() {
+        try {
+            String uriString = "content://com.example.sqltestfinish/person";
+            Uri uri = new Uri.Builder().build().parse(uriString);
+
+            String[] columns = new String[] {"name", "age", "mobile"};
+            Cursor cursor = getContentResolver().query(uri, columns, null, null, "name ASC");
+            println("query res : " + cursor.getCount());
+
+            int index = 0;
+            while(cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(columns[0]));
+                int age = cursor.getInt(cursor.getColumnIndex(columns[1]));
+                String mobile = cursor.getString(cursor.getColumnIndex(columns[2]));
+
+                println("#" + index + " -> " + name + ", " + age + ", " + mobile);
+                index += 1;
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updatePerson() {
+        String uriString = "content://com.example.sqltestfinish/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        String selection = "mobile = ?";
+        String[] selectionArgs = new String[] {"010-3333-3333"};
+        ContentValues updateValue = new ContentValues();
+        updateValue.put("mobile", "010-7777-7777");
+
+        int count = getContentResolver().update(uri, updateValue, selection, selectionArgs);
+        println("update res : " + count);
+    }
+
+    public void deletePerson() {
+        String uriString = "content://com.example.sqltestfinish/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        String selection = "name = ?";
+        String[] selectionArgs = new String[] {"Jessica"};
+
+        int count = getContentResolver().delete(uri, selection, selectionArgs);
+        println("delete res : " + count);
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+}
